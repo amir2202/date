c8321d37fca4a8db12b09bd98df90c1a909bf3c2
flyingmat <paramattimatteo@gmail.com>
2020-07-02 21:59:09 +0100

    BUTTONS GO BRR

--- lib/main.dart
+++ lib/main.dart
@@ -4,35 +4,75 @@ void main() => runApp(new Dating());
 
 class Dating extends StatelessWidget {
   Widget build(BuildContext context){
-    Container background = new Container(
+
+    Container backgroundGradient = new Container(
         decoration: new BoxDecoration(
-            gradient: new LinearGradient(colors: [const Color(0xFF915FB5), const Color(0xFFCA436B)],
+            gradient: new LinearGradient(
+              colors: [const Color(0xFF915FB5), const Color(0xFFCA436B)],
               begin: FractionalOffset.topLeft,
               end: FractionalOffset.bottomRight,
-              stops:[0.0,1.0],
+              stops: [0.0,1.0],
               tileMode: TileMode.clamp,
             )
         )
     );
 
 
-    OutlineButton login = new OutlineButton(onPressed: () {},
-        child: Text('LOG IN',style: TextStyle(color: Colors.white)),highlightedBorderColor: Colors.white, borderSide: BorderSide(color: Colors.white,width:1.0),);
-    RaisedButton register = new RaisedButton(onPressed: () {},
-        child: Text('SIGN UP'),color: Colors.white,);
-    Column buttoncol = new Column(mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,children: <Widget>[login,register]);
+    OutlineButton loginButton = new OutlineButton(
+      onPressed: () {},
+      child: Text(
+        'LOG IN',
+        style: TextStyle(color: Colors.white)
+      ),
+      highlightedBorderColor: Colors.white,
+      borderSide: BorderSide(color: Colors.white,width:1.0),
+      shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+    );
+
+    RaisedButton signupButton = new RaisedButton(
+      onPressed: () {},
+      child: Text('SIGN UP'),
+      color: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+    );
+
+    SizedBox loginSizedBox = new SizedBox(
+      width: 200,
+      height: 50,
+      child: loginButton,
+    );
+
+    SizedBox signupSizedBox = new SizedBox(
+      width: 200,
+      height: 50,
+      child: signupButton,
+    );
+
+    Column buttonColumn = new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        loginSizedBox,
+        new SizedBox(
+          width: 0,
+          height: 10,
+          child: null,
+        ),
+        signupSizedBox,
+      ],
+    );
 
     //DropdownButton<String> languages = new DropdownButton<String>();
 
     return new MaterialApp(
-
-        home: new Stack(
-            alignment:Alignment.center,
-            children: <Widget>[
-          background,buttoncol
-        ])
-        );
+      home: new Stack(
+        alignment:Alignment.center,
+        children: <Widget>[
+          backgroundGradient,
+          buttonColumn,
+        ],
+      )
+    );
   }
 
 }
