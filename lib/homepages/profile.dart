d4c8ad5c3a7e57a8b38b784747d99f956a26742f
flyingmat <paramattimatteo@gmail.com>
2020-07-08 16:56:40 +0100

    OMG I FORGOT RIP

---
+++ lib/homepages/profile.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:date/common.dart';
+
+class ProfilePage {
+  static Widget profilePage() {
+
+    return Stack(
+      children: <Widget>[
+
+        Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: Common.screenHeight * 0.2,
+                color: Color(0xFFCA436B),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                ),
+              ),
+            ]
+        ),
+
+        Positioned(
+          top: Common.screenHeight * 0.1,
+          left: Common.screenWidth * 0.05,
+          child: Hero(
+            tag: 'preview_tag',
+            child: SizedBox(
+              width: Common.screenWidth * 0.9,
+              height: 150,
+              child: Card(
+                elevation: 10,
+                color: Colors.white,
+                child: Container(
+                  padding: EdgeInsets.all(40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Material(
+                        elevation: 4,
+                        shape: CircleBorder(),
+                        clipBehavior: Clip.hardEdge,
+                        color: Colors.transparent,
+                        child: Ink.image(
+                          image: FileImage(Common.profilePicture),
+                          fit: BoxFit.cover,
+                          width: 100,
+                          height: 100,
+                          child: InkWell(
+                            onTap: () {},
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
