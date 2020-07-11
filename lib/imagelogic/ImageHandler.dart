0ae0623c96a924360aa5d7da43a284d0f3f375ac
amir2202 <aag1u18@soton.ac.uk>
2020-07-11 18:58:46 +0100

    image upload complete

--- lib/imagelogic/ImageHandler.dart
+++ lib/imagelogic/ImageHandler.dart
@@ -5,21 +5,20 @@ import 'package:dating/GraphQLHandler.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 class ImageHandler{
 
-  static Future<bool> uploadImage(File file, String userid) async {
+  static Future<bool> uploadImage(File file, String userid,bool profile) async {
     bool upload = false;
     final bytes = await file.readAsBytes();
     String img64 = base64Encode(bytes);
-    const String mutation = r"""mutation ImgM($img:String!, $userid:String!){
-  upload(img:$img,userid:$userid)
+    const String mutation = r"""mutation ImgM($img:String!, $userid:String!,$pfp:Boolean!){
+  upload(img:$img,userid:$userid,pfp:$pfp)
   }
   """;
     GraphQLClient client = GraphQLClient(cache: InMemoryCache(), link: HttpLink(uri:'http://192.168.0.14:8090/graphql'));
-    client.mutate(MutationOptions(documentNode:gql(mutation),variables: {'img':img64,'userid':userid}));
+    client.mutate(MutationOptions(documentNode:gql(mutation),variables: {'img':img64,'userid':userid,'pfp':profile}));
 
 
     print("Doing stuff");
     return upload;
   }
-
-
+  
 }
