ededd8d8aec594387c9b702b465add4bf97e64f1
amir2202 <aag1u18@soton.ac.uk>
2020-07-25 00:11:34 +0100

    chat page updated

--- lib/homepages/ChatPage.dart
+++ lib/homepages/ChatPage.dart
@@ -1,14 +1,13 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
-  State<StatefulWidget> createState() {
-
-    throw UnimplementedError();
-  }
+  ChatPageState createState() => ChatPageState();
 
 }
 
@@ -17,21 +16,39 @@ class ChatPageState extends State<ChatPage> {
   void initState(){
 
   }
+  Socket socket;
+  void test() async {
+    final client = await Socket.connect('192.168.0.30', 9999);
+    print("connected");
+    client.add(utf8.encode('123\n'));
+    await client.flush();
+    client.listen(
+            (var data) => print('Got $data'),
+        onDone: () { print('Done'); client.close(); },
+        onError: (e) { print('Got error $e'); client.close(); });
+    print('main done');
+  }
 
-  void test() async{
-    Socket socket = await Socket.connect("54.37.205.205", 9999);
-    socket.write("53");
-    socket.listen((List<int> event) {
-      print(utf8.decode(event));
-    });
+  void dataHandler(data){
+    print(new String.fromCharCodes(data).trim());
+  }
+
+  void errorHandler(error, StackTrace trace){
+    print(error);
+  }
+
+  void doneHandler(){
+    socket.destroy();
+    exit(0);
   }
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: FlatButton.icon(onPressed:() {
+      child: RaisedButton(onPressed: (){
+        print("test");
         test();
-
-      }, icon: null, label: Text('hi')) ,
+      },textColor: Colors.white,child: const Text('Gradient Button', style: TextStyle(fontSize: 20)),
+      ),
     );
   }
   
